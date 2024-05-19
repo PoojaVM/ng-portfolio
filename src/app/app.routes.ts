c8b1206808fa2e22fae9e938ba355aa807f5e28f
Pooja Mule <poojavm1595@gmail.com>
2024-05-18 23:07:16 -0400

    Routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/', pathMatch: 'full'},
+    {path: '/about-me', component: AppComponent},
+    {path: '/work-experience', component: AppComponent},
+    {path: '/projects', component: AppComponent},
+    {path: '/skills', component: AppComponent},
+    {path: '/contact-me', component: AppComponent},
+];
